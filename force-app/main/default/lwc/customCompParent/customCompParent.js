debf0c6e2e1a5927a33bad912a4224a7c6b595b9
Dinesh <dineshbaddawar@gmail.com>
2022-07-01 23:57:14 +0530

    commit

--- force-app/main/default/lwc/customCompParent/customCompParent.js
+++ force-app/main/default/lwc/customCompParent/customCompParent.js
@@ -2,6 +2,12 @@ import { LightningElement,track } from 'lwc';
 
 export default class CustomCompParent extends LightningElement {
      @track message;
+
+     constructor(){
+          super();
+          this.template.addEventListener('mycustomevent')
+     }
+
      handleCustomEvent(event){
           const textVal =event.detail;
           this.message = textVal;
